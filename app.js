7edb13041473c7a0069dc900818b14ddc3a081ea
Mike Molenda <molenda.mike@gmail.com>
2017-06-20 08:11:55 -0500

    Set up basic express server

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-var express = require('express');
-var path = require('path');
-var bodyParser = require('body-parser');
-var nodeMailer = require('nodemailer');
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const nodeMailer = require('nodemailer');
 
+const app = express();
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.listen(port);
+console.log(`Server running on port ${port}`);
